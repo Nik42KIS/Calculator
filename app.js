ff6c0d3ff311f5033fee5c57e4a5fc14b80e95e4
Nikita K <rabot422@gmail.com>
2023-09-27 22:13:07 +0300

    a bit clean code from console.log and other

--- app.js
+++ app.js
@@ -1,69 +1,65 @@
 let firstNum = '';
 let secondNum = '';
 let curSign = null;
-const equal = document.querySelector('.equal')
+const equal = document.querySelector('.equal');
 const digit = document.querySelectorAll('.number');
 const operator = document.querySelectorAll('.operator');
-const reset = document.querySelector('.reset')
-const out = document.querySelector('.display__out')
+const reset = document.querySelector('.reset');
+const out = document.querySelector('.display__out');
 let flag = false;
 
 digit.forEach((el) => {
   el.addEventListener('click', () => {
     if (flag) {
-        secondNum += el.textContent;
-        out.textContent += el.textContent;
-      console.log(firstNum, secondNum);
+      secondNum += el.textContent;
+      out.textContent += el.textContent;
     } else {
       firstNum += el.textContent;
-      out.textContent = firstNum
-      console.log(firstNum, secondNum);
+      out.textContent = firstNum;
     }
   });
 });
-operator.forEach((el) =>{
-    el.addEventListener('click', () =>{
-        curSign = el.textContent
-        out.textContent += el.textContent
-        flag = true
-    })
-})
+operator.forEach((el) => {
+  el.addEventListener('click', () => {
+    curSign = el.textContent;
+    out.textContent += el.textContent;
+    flag = true;
+  });
+});
 
 reset.addEventListener('click', () => {
-    firstNum = '';
-    secondNum = '';
-    out.textContent = 0
-    flag = false;
-    console.log(`firstNum : ${firstNum}`,  `secondNum : ${secondNum}`)
-})
+  firstNum = '';
+  secondNum = '';
+  out.textContent = 0;
+  flag = false;
+});
 
 function operate(oneN, twoN, sign) {
-    let result = 0;
-    let first = Number(oneN);
-    let second = Number(twoN);
-    
-    switch(sign){
-        case '/': 
-            result = first / second;
-            break;
-        case '+':
-             result = first + second;
-             break;
-        case '*': 
-            result = first * second;
-            break;
-        case '-': 
-            result = first - second;
-            break;
-        default: result = 'error'
-    }
-    return result
-    
+  let result = 0;
+  let first = Number(oneN);
+  let second = Number(twoN);
+
+  switch (sign) {
+    case '/':
+      result = first / second;
+      break;
+    case '+':
+      result = first + second;
+      break;
+    case '*':
+      result = first * second;
+      break;
+    case '-':
+      result = first - second;
+      break;
+    default:
+      result = 'error';
+  }
+  return result;
 }
 
-equal.addEventListener('click', () =>{
-    firstNum = operate(firstNum, secondNum, curSign)
-    out.textContent = firstNum;
-    secondNum = ''
-    console.log(firstNum)
-})
+equal.addEventListener('click', () => {
+  firstNum = operate(firstNum, secondNum, curSign);
+  out.textContent = firstNum;
+  secondNum = '';
+});
